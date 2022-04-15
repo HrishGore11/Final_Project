c342375dbbf1fb54f4675e9faa910232cea87576
hrishgore11 <hrishgore11@gmail.com>
2022-04-15 18:13:06 +0530

    day 3 - login api made

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,13 @@ const router = express.Router();
 const User = require("../models/user");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const jwt_key = "sdfghjkla@$%&";
 
 // const { create } = require("../models/user");
+
+////////////////////////////////////signUp Route:
+/////////////////////////http://localhost:9046/api/auth/signUp
 router.post(
   "/signUp",
 
@@ -43,10 +48,57 @@ router.post(
       createUser.save();
       //   console.log(req.body);
       res.json({ message: "succesfully created User", data: createUser });
-    } catch {
-      (err) => {
-        console.log(err);
+    } catch (error) {
+      console.error(errors);
+      res.status(500).send("internal server error");
+    }
+  }
+);
+//////////////////////////////////////////////////////////////////login Route :
+///////////////////////// http://localhost:9046/api/auth/login
+
+router.post(
+  "/login",
+  [
+    body("email", "please enter a valid email ").isEmail(),
+    body("password", "please enter a valid password ").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({
+        email: email,
+      });
+      if (!user) {
+        return res
+          .status(200)
+          .json({ success: false, message: "Please enter registered email" });
+      }
+      const passwordcompare = await bcrypt.compare(password, user.password);
+      if (!passwordcompare) {
+        return res
+          .status(401)
+          .json({ success: false, message: "incorrect Password" });
+      }
+      const payload = {
+        user: {
+          id: user._id,
+        },
       };
+      const authtoken = jwt.sign(payload, jwt_key);
+
+      res.json({
+        message: "Token created succesfully",
+        data: authtoken,
+      });
+      console.log(authtoken);
+    } catch (error) {
+      console.error(errors);
+      res.status(500).send("internal server error");
     }
   }
 );
